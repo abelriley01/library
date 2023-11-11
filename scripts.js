f749b8008f2127d38a095f026d3340bfa22154b6
Abel Tennison-Riley <attennisonriley@gmail.com>
2023-11-11 21:45:45 +0000

    add popup form:

--- scripts.js
+++ scripts.js
@@ -1,16 +1,15 @@
-const addBookbutton = document.getElementById('addBook');
-const modal = document.getElementById('myModal');
-const openButton = document.getElementById("openButton");
-const closeButton = document.getElementById("closeButton");
+
+let myLibrary= [
+    {
+        title: "",
+        author: "",
+        pages: "",
+        read: true
+    }
+];
 
 
-
-
-
-const myLibrary= [];
-
-
-function Book(title, author, pages, read){
+function book(title, author, pages, read){
     this.title = title
     this.author = author
     this.pages = pages
@@ -20,22 +19,47 @@ function Book(title, author, pages, read){
     }
 }
 
-function addBookToLibrary(){
+function buildLibraryBox(array){
+    const outputDiv = document.getElementById("output");
 
-}
+    array.forEach(function(book){
+        const tile = document.createElement("div");
 
-addBookbutton.addEventListener('click',addBookToLibrary);
+        tile.textContent= book.info();
 
-openButton.addEventListener("click", function(){
-    modal.style.display ="block";
-});
-
-closeButton.addEventListener("click", function(){
-    modal.style.display = "none";
-});
-
-window.addEventListener("click" , function(event){
-    if(event.target === modal){
-        modal.style.display = "none":
+        outputDiv.appendChild(tile);
+        });
     }
+
+buildLibraryBox(myLibrary);
+
+
+  document.getElementById("openForm").addEventListener("click", function() {
+    document.getElementById("popupForm").style.display = "block";
+  });
+
+
+  document.getElementById("closeForm").addEventListener("click", function() {
+    document.getElementById("popupForm").style.display = "none";
+  });
+
+document.getElementById("myForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+
+    buildLibraryBox(myLibrary);
+
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+    document.getElementById("read").checked = false;
+
+    document.getElementById("popupForm").style.display = "none";
 });
